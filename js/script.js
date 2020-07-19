29ad8cf796917ec0503b3e573d82ff195c598f27
DestinTech <destintech@gmail.com>
2020-07-19 14:10:50 -0600

    create a css rule for container width

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 const container = document.querySelector("#container");
 
 //create a div/grid generating function to fill the container with eonugh divs. 
-const makeGrid = (width,height) =>{
-    let totalDiv = width * height;
+const makeGrid = (gridWidth,gridHeight) =>{
+    let totalDiv = gridWidth * gridHeight;
     let divArray = [];
+    
+    gridWidth = `${gridWidth * 20}px`; 
+    console.log(gridWidth);
+    container.style.width = gridWidth; //"400px";//set the container width calculated by pixelWidth * width
 
-    for (let i=0; i <= totalDiv; i++){
+    for (let i=0; i < totalDiv; i++){
         divArray[i] = document.createElement('div'); //create a new div
         divArray[i].id = "grid-"+ i; //add an ID to the div
         divArray[i].className = "gridDiv";
         divArray[i].style.width = "20px";
         divArray[i].style.height = "20px";
         container.appendChild(divArray[i]); //add the div to the container
+    
     }
+    
 }
 
 makeGrid(20,20);
